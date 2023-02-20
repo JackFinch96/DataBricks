b64deb280474cb6b4428c28740f89af78d5caa85
JackFinch96 <JackFinch96>
2023-02-20 14:21:17 +0000

    Added some basic Python

---
+++ Day1PM.py
@@ -0,0 +1,44 @@
+# Databricks notebook source
+print("Hello World")
+
+# COMMAND ----------
+
+#stings
+machine_name = "AKEA L"
+
+print(machine_name)
+
+#booleans
+bool1 = True
+bool2 = False
+
+print(bool2)
+    
+#integer
+int1 = 4296
+int2 = 1657
+
+print(int1 , int2)
+
+#float
+float1 = 28.8
+    
+print(float1)
+
+#list
+list1 = [30,33,36,42,28]
+list_products = ["AKEA", "ORT", "DELTA LINE"]
+    
+print(list1)
+print(list_products)
+
+# COMMAND ----------
+
+number1 = input("please enter a number")
+number2 = input("enter a second")
+
+print(int(number1) + int(number2))
+
+# COMMAND ----------
+
+
